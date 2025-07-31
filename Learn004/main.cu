c66596e8bef0953668dd383c83d614f2a50ac337
tsxy2007 <z.tk@163.com>
2025-07-31 11:25:55 +0800

    1.完成矩阵的乘法计算，初级版

--- Learn004/main.cu
+++ Learn004/main.cu
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstdlib>
-
+/**
+* 求矩阵的乘积
+*/
 struct Matrix
 {
     Matrix() {}
@@ -13,18 +15,18 @@ struct Matrix
 
 #define BLOCK_SIZE 16
 
-__global__ void MatrixMulti(Matrix A,Matrix B,Matrix C)
+__global__ void MatrixMulti(float* A, int A_W, int A_H,float* B, int B_W, int B_H, int C_W, float* C )
 {
     int row = blockDim.x * blockIdx.x  + threadIdx.x;
     int col = blockDim.y * blockIdx.y + threadIdx.y;
-    if (row < A.mWidth && col < B.mHeight)
+    if (row < A_W && col < B_H)
     {    
         float Cvalue = 0;
-        for (int i = 0; i < A.mWidth; i++)
+        for (int i = 0; i < A_W; i++)
         {
-            Cvalue += A.Data[row * A.mWidth + i] * B.Data[i * B.mWidth + col];
+            Cvalue += A[row * A_W + i] * B[i * B_W + col];
         }
-        C.Data[row * C.mWidth + col] = Cvalue;
+        C[row * C_W + col] = Cvalue;
     }
     
 }
@@ -36,15 +38,20 @@ int main(int argc,char** argv)
     Matrix h_A;
     Matrix h_B;
     h_A.mWidth = w;
-    h_A.mWidth = h;
+    h_A.mHeight = h;
+    size = h_A.mWidth * h_A.mHeight * sizeof(float);
     h_A.Data = (float*)malloc(size);
+
     h_B.mHeight = h;
-    h_B.mWidth = w;
+    h_B.mWidth = w;    
+    size = h_B.mWidth * h_B.mHeight * sizeof(float);
     h_B.Data = (float*)malloc(size);
 
     Matrix h_C;
     h_C.mWidth = h_A.mWidth;
     h_C.mHeight = h_B.mHeight;
+    size = h_C.mWidth * h_C.mHeight * sizeof(float);
+    h_C.Data = (float*)malloc(size);
 
     for (int i = 0; i < w; i++)
     {
@@ -60,7 +67,7 @@ int main(int argc,char** argv)
                 h_A.Data[Index] = 0.f;
             }
             h_B.Data[Index] = (double)rand() / (RAND_MAX +1.0);
-            printf("h_A[%d][%d] = %f",i,j,h_A.Data[Index]);
+            printf("h_B[%d][%d] = %f; ",i,j,h_B.Data[Index]);
         }
         printf("\n");
     }
@@ -70,7 +77,7 @@ int main(int argc,char** argv)
     size = h_A.mWidth * h_A.mHeight * sizeof(float);
     cudaMalloc(&d_A.Data, size);
     cudaMemcpy(d_A.Data, h_A.Data, size, cudaMemcpyHostToDevice);
-     
+    
     Matrix d_B;
     d_B.mWidth = h_B.mWidth;
     d_B.mHeight = h_B.mHeight;
@@ -78,7 +85,7 @@ int main(int argc,char** argv)
     cudaMalloc(&d_B.Data, size);
     cudaMemcpy(d_B.Data, h_B.Data, size, cudaMemcpyHostToDevice);
 
-
+    
     // Allocate C in device memory
     Matrix d_C;
     d_C.mWidth = h_C.mWidth; d_C.mHeight = h_C.mHeight;
@@ -86,15 +93,27 @@ int main(int argc,char** argv)
     cudaMalloc(&d_C.Data, size);
 
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid(h_A.mWidth / dimBlock.x, h_B.mHeight / dimBlock.y);
-    MatrixMulti <<<dimGrid, dimBlock >>> (d_A, d_B, d_C);
+    dim3 dimGrid((h_A.mWidth + dimBlock.x -1 )/ dimBlock.x, (h_B.mHeight + dimBlock.y - 1)/ dimBlock.y);
+    MatrixMulti <<<dimGrid, dimBlock >>> (d_A.Data,d_A.mWidth,d_A.mHeight, d_B.Data,d_B.mWidth,d_B.mHeight,d_C.mWidth, d_C.Data);
 
     cudaMemcpy(h_C.Data, d_C.Data, size,
         cudaMemcpyDeviceToHost);
+
+    printf("-----------------------------------------------------------------------------\n");
+    for (int i = 0; i < w; i++)
+    {
+        for (int j = 0; j < h; j++)
+        { 
+            int Index = i * w  + j;
+            printf("h_C[%d][%d] = %f; ",i,j,h_C.Data[Index]);
+        }
+        printf("\n");
+    }
 
     // Free device memory
     cudaFree(d_A.Data);
     cudaFree(d_B.Data);
     cudaFree(d_C.Data);
+    
     return 0;
 }
